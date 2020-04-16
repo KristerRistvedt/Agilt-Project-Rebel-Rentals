e35453cefe79ed271ac8e3546cde689589c02a8f
Alexander Molnar <molex90@gmail.com>
2020-04-16 12:34:25 +0200

    WEB1-46 Created page from preset and added ship model

---
+++ RebelRentals/RebelRentals/Pages/Ships/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RebelRentals.Data;
+using RebelRentals.Models;
+
+namespace RebelRentals.Pages.Ships
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RebelRentals.Data.ApplicationDbContext _context;
+
+        public IndexModel(RebelRentals.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Ship> Ship { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Ship = await _context.Ship.ToListAsync();
+        }
+    }
+}
