64d132908cb3800d4d8d383d662f26d3569af6f4
Sunse7 <sunsetluna@gmail.com>
2020-04-17 16:17:47 +0200

    WEB1-56 Created ShoppingCartOverview page and makes it show shopping cart content also includes WEB1-54

--- RebelRentals/RebelRentals/Data/ShoppingCart.cs
+++ RebelRentals/RebelRentals/Data/ShoppingCart.cs
@@ -8,7 +8,35 @@ namespace RebelRentals
 {
     public class ShoppingCart
     {
-        private List<Ship> shoppingCart = new List<Ship>();
+        private List<Ship> shoppingCart = new List<Ship>
+        {
+            new Ship
+            {
+                Model = "A-Wing",
+                Class = "Very Fighter",
+                Height = 1,
+                Length = 1,
+                Width = 1,
+                Price = 100,
+                NumberOfPopulation = 1,
+                MaxSpeed = 1000
+            },
+            new Ship
+            {
+                Model = "X-Wing",
+                Class = "Very Fighter",
+                Height = 1,
+                Length = 1,
+                Width = 1,
+                Price = 100,
+                NumberOfPopulation = 1,
+                MaxSpeed = 1000
+            }
+        };
+        public List<Ship> GetShoppingList()
+        {
+            return shoppingCart;
+        }
 
         public void AddShipToCart(Ship ship)
         {
---
+++ RebelRentals/RebelRentals/Pages/ShoppingCartOverview.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RebelRentals.Data;
+using RebelRentals.Models;
+
+namespace RebelRentals.Pages
+{
+    public class ShoppingCartOverviewModel : PageModel
+    {
+        private readonly RebelRentals.Data.ApplicationDbContext _context;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartOverviewModel(RebelRentals.Data.ApplicationDbContext context, ShoppingCart shoppingCart)
+        {
+            _context = context;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IList<Ship> Ship { get;set; }
+       
+        public async Task OnGetAsync()
+        {
+            Ship = _shoppingCart.GetShoppingList().ToList();
+        }
+    }
+}
