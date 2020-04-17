6378cda0c969e1ca3ac647a2155b3313f44ba039
Elvis <elvostudios@hotmail.com>
2020-04-17 15:30:04 +0200

    WEB1-55 Created class with implemented base functionality

---
+++ RebelRentals/RebelRentals/Data/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using RebelRentals.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RebelRentals
+{
+    public class ShoppingCart
+    {
+        private List<Ship> shoppingCart = new List<Ship>();
+
+        public void AddShipToCart(Ship ship)
+        {
+            shoppingCart.Add(ship);
+        }
+
+        public void RemoveShipFromCart(Ship ship)
+        {
+            shoppingCart.Remove(ship);
+        }
+    }
+}
