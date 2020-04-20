148a94c759ff14b34c35fc4af1f18d299dc8c0a1
Alexander Molnar <molex90@gmail.com>
2020-04-20 10:56:45 +0200

    WEB1-59 Created an order page

---
+++ RebelRentals/RebelRentals/Pages/OrderPage/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RebelRentals.Data;
+using RebelRentals.Models;
+
+namespace RebelRentals.Pages.OrderPage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RebelRentals.Data.ApplicationDbContext _context;
+        public List<Ship> ShoppingCart { get; set; }
+
+        public IndexModel(RebelRentals.Data.ApplicationDbContext context, List<Ship> shoppingCart)
+        {
+            _context = context;
+            ShoppingCart = shoppingCart;
+        }
+
+        public async Task OnGetAsync()
+        {
+            ShoppingCart = await _context.Ship.ToListAsync();
+        }
+    }
+}
