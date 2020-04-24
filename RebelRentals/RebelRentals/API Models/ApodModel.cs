c330a5db8b1ad6c0ff881c4daa70ae35b468b6b4
TheEsquimo <elvostudios@hotmail.com>
2020-04-24 14:08:41 +0200

    WEB1-90 ApodModel class added

---
+++ RebelRentals/RebelRentals/API Models/ApodModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RebelRentals.API_Models
+{
+    public class ApodModel
+    {
+        public string ApodSite { get; set; }
+        public string Copyright { get; set; }
+        public string Music { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string MediaType { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
