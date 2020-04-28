5dc208f9afc3b0c215f6db1f24a8a498c67eb0ab
Elvis <elvostudios@hotmail.com>
2020-04-28 10:22:03 +0200

    WEB1-105 Created Contact page

---
+++ RebelRentals/RebelRentals/Pages/Contact.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RebelRentals.Data;
+
+namespace RebelRentals
+{
+    public class ContactModel : PageModel
+    {
+        private ApplicationDbContext context;
+        public ContactModel(ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
