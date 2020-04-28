e34d19b54dbf0876de9374cdaf4277649e6db0a5
Elvis <elvostudios@hotmail.com>
2020-04-28 11:57:03 +0200

    WEB1-106 WEB1-142 Added navbar button that leads to Contact page, added input fields for mail function

--- RebelRentals/RebelRentals/Pages/Contact.cshtml.cs
+++ RebelRentals/RebelRentals/Pages/Contact.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,16 @@ namespace RebelRentals
     public class ContactModel : PageModel
     {
         private ApplicationDbContext context;
+
+        [BindProperty]
+        [Display(Name = "Name")]
+        public string NameField { get; set; }
+        [BindProperty]
+        [Display(Name = "Email")]
+        public string EmailField { get; set; }
+        [BindProperty]
+        [Display(Name = "Message")]
+        public string MessageField { get; set; }
         public ContactModel(ApplicationDbContext context) 
         {
             this.context = context;
