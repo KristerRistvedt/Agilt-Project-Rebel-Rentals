334168b4035e5ed228a46f09683111e0bb9d1efe
Alexander Molnar <molex90@gmail.com>
2020-05-12 08:36:08 +0200

    back up

---
+++ RebelRentals/RebelRentals/API Models/ChuckNorrisQuotesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RebelRentals.API_Models
+{
+    public class ChuckNorrisQuotesModel
+    {
+        public string IconURL { get; set; }
+        public string Id { get; set; }
+        public string Quote { get; set; }
+    }
+}
---
+++ RebelRentals/RebelRentals/Pages/ChuckNorrisQuotes.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
+
+namespace RebelRentals.Pages
+{
+    public class ChuckNorrisQuotesModel : PageModel
+    {
+        [BindProperty]
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "If you're sad and need a Chuck Norris quote to cheer you up, click this button:";
+        }
+
+        public void OnPostGetQuote()
+        {
+             
+        }
+    }
+}
