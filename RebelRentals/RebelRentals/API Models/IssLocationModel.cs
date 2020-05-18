404950127d473534ef75f312cc00306165eda3a2
Sunse7 <sunsetluna@gmail.com>
2020-05-18 14:32:34 +0200

    WEB1-203 Created Model class for ISS API

---
+++ RebelRentals/RebelRentals/API Models/IssLocationModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RebelRentals.API_Models
+{
+    public class IssLocationModel
+    {
+        public IssLocation Location { get; set; }
+
+        public partial class IssLocation
+        {
+            [JsonProperty("timestamp")]
+            public long Timestamp { get; set; }
+
+            [JsonProperty("message")]
+            public string Message { get; set; }
+
+            [JsonProperty("iss_position")]
+            public IssPosition IssPosition { get; set; }
+        }
+
+        public partial class IssPosition
+        {
+            [JsonProperty("latitude")]
+            public string Latitude { get; set; }
+
+            [JsonProperty("longitude")]
+            public string Longitude { get; set; }
+        }
+    }
+}
